bf21ff9be8592c56be9701e314461f0396a42038
BadTrippp <nqviet96@gmail.com>
2022-10-18 16:41:31 -0400

    update

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,31 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+echo "========= CLEAR OLD ENVIRONMENT ========="
+docker stop project4
+docker rm project4
+docker rmi project4
+echo "========= DONE ========="
+echo "." 
+echo "."
+echo "."
+echo "========= BUILD NEW IMAGE ========="
+docker build --tag=project4 .
+echo "========= DONE ========="
+echo "." 
+echo "."
+echo "."
 # Step 2: 
 # List docker images
-
+echo "========= LIST IMAGES ========="
+docker image ls
+echo "========= DONE ========="
+echo "." 
+echo "."
+echo "."
 # Step 3: 
 # Run flask app
+echo "========= START ========="
+docker run --name project4 -dp 8000:80 project4 
+echo "========= DONE ========="
+# docker logs project4 -f >> docker_out.txt 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=badtrip/udacity-project4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login registry-1.docker.io
+docker tag project4 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push badtrip/udacity-project4
